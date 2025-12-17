dc14e476424735be8bbe5f4f71dc0c61567954f5
bmm333 <benmema3@gmail.com>
2025-12-17 04:50:27 -0500

    chore: project setup

---
+++ msvm-studio/src/main/java/it/uniupo/msvm/App.java
@@ -0,0 +1,13 @@
+package it.uniupo.msvm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
