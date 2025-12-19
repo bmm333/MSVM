b32f8265fa42db2f1d12ebb70e6bd80111edf591
bmm333 <benmema3@gmail.com>
2025-12-19 10:35:15 -0500

    MSVM-4

--- msvm-studio/src/main/java/it/uniupo/msvm/core/cpu/Cpu.java
+++ msvm-studio/src/main/java/it/uniupo/msvm/core/cpu/Cpu.java
@@ -8,6 +8,11 @@ import it.uniupo.msvm.core.memory.OperandStack;
 import che servono per il funzionamento del cpu
 Ancora da definire TODO @lucalupi -> Memory
 */
+import java.util.Map;
+import java.util.HashMap;
+
+
+
 public class CPU{
     private final Memory memory;
     private final OperandStack stack;
@@ -23,8 +28,49 @@ public class CPU{
         this.memory = memory;
         this.stack = stack;
     }
-    //Bisognia di un metodo che reg una nuova istr nel set del cpu in modo tale da rispettare Open Closed Principle OCP
-    // Metodo che esegue tutto il ciclo fino a -> Halt || Error
-    //Metodo Step (OP atomiche f->d->e)
-    //e i getters e setters
+    public void registerInstruction(OpCode opcdoe,Instruction implementation)
+    {
+        instructionSet.put(opcdoe,implementation);
+    }
+    //Esegue tutto fino a halt oppure Errore
+    public void run()
+    {
+        while(!isHalted)
+        {
+            step();
+        }
+    }
+
+    //Eseguira un step atomico (1.Fetch->2.Decode->3.Execute)
+    public void step()
+    {
+        if(isHalted) return;
+
+        //1.Fetch
+        //Se passa lanciera un eccezione di tipo Address out of bounds (Guarda key MSVM-5 Jira)
+        if(ip>=memory.sizeMemory())
+        {
+            throw new RuntimeException("Segmentation fault\n");
+        }
+        int opcodeByte=memory.read(ip);
+        ip++; //incr puntatore
+        //2.Decode
+        Opcode opcode;
+        try{
+            opcode=Opcode.fromByte(opcodeByte); 
+        }catch(IllegalArgumentException e)
+        {
+            throw new RuntimeException("Illegal Instruction on Address:" + ip-1);
+        }
+        Instruction instruction=instructionSet.get(opcode);
+        if(!instruction)
+        {
+            throw new RuntimeException("Not implemented");
+        }
+        instruction.execute(this,memory,stack);
+    }
+
+    public void halt(){this.isHalted=true;}
+    public int getIp(){return this.ip;}
+    public int setIp(int ip){this.ip=ip;}
 }
