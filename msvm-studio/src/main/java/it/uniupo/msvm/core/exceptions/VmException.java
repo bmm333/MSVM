b527d9a4d7f8379bc10fd7bf089bd1d231e27fdf
bmm333 <benmema3@gmail.com>
2025-12-23 20:18:19 +0100

    MSVM-5 : chore: Javadoc

--- msvm-studio/src/main/java/it/uniupo/msvm/core/exceptions/VmException.java
+++ msvm-studio/src/main/java/it/uniupo/msvm/core/exceptions/VmException.java
@@ -2,7 +2,22 @@ package it.uniupo.msvm.core.exceptions;
 
 import it.uniupo.msvm.exceptions.MsvmException;
 
+/**
+ * Classe base astratta per tutti gli errori di esecuzione della Virtual Machine.
+ * <p>
+ *     Ragruppatutte le anomalie che possono verificarsi durante il ciclo fetch-decode-execute.
+ * (es. violazione memoria, stack overflow, opcode illegale).
+ * Il {@code VmRunner} catturera questa classe generica per fermare l'esecuzione.
+ * e notificera l'interfacia utente in caso di crash.
+ *  </p>
+ * */
+
 public class VmException extends MsvmException {
+    /**
+     * Costruisce un'eccezione relativa al runtime della VM.
+     *
+     * @param message Descrizione dell'errore di esecuzione.
+     */
     public VmException(String message) {
         super(message);
     }
