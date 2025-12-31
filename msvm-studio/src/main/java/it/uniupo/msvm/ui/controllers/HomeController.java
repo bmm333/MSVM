beb04c4fe3f8dc10d7850ca7adc8a58e0b11fcbd
Luca Lupi <168643943+Ilpupi00@users.noreply.github.com>
2025-12-31 15:49:17 +0100

    Add homepage

---
+++ msvm-studio/src/main/java/it/uniupo/msvm/ui/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package it.uniupo.msvm.ui.controllers;
+
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+    @FXML
+    public void inizialize(){
+        System.out.println("Homepage Initialization");
+    }
+
+    @FXML
+    public void handleCreaFile(){
+        System.out.println("Click su Crea File");
+        // TODO: Aprire la vista Editor vuota
+    }
+
+    @FXML
+    public void handleApriFile() {
+        System.out.println("Click su Apri File");
+        // TODO: Aprire il FileChooser di JavaFX
+    }
+}
