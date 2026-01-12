6e17cc590ffa676285a1d5448573d454a92b4679
bmm333 <benmema3@gmail.com>
2026-01-12 23:07:07 +0100

    user dao

---
+++ msvm-studio/src/main/java/it/uniupo/msvm/server/persistence/DatabaseManager.java
@@ -0,0 +1,4 @@
+package it.uniupo.msvm.server.persistence;
+
+public class DatabaseManager {
+}
---
+++ msvm-studio/src/main/java/it/uniupo/msvm/server/persistence/dao/UserDAO.java
@@ -0,0 +1,45 @@
+package it.uniupo.msvm.server.persistence.dao;
+
+import it.uniupo.msvm.common.model.User;
+import it.uniupo.msvm.server.persistence.DatabaseManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Optional;
+
+public class UserDAO {
+    private final DatabaseManager dbManager;
+
+    public UserDAO() {
+        this.dbManager = new DatabaseManager();
+    }
+    /**
+     * Cerca un utente per mail
+     * Grazie al indice 'idx_user_email', questa operazione e O(logN)
+     * */
+    public Optional<User> findByEmail(String email)
+    {
+        String sql="SELECT id,username,email,password FROM users WHERE email=?";
+        try(Connection conn=dbManager.getConnection();
+            PreparedStatement pstmt=conn.prepareStatement(sql)){
+            pstmt.setString(1,email);
+            try(ResultSet rs=pstmt.executeQuery()){
+                if(rs.next())
+                {
+                    User user=new User(
+                            rs.getLong("id"),
+                            rs.getString("username"),
+                            rs.getString("password"),
+                            rs.getString("email")
+                    );
+                    return Optional.of(user);
+                }
+            }
+            catch (Exception e) {
+                System.err.println("[DAO] Error finding userby email:" + e.getMessage());
+            }
+        }
+        return Optional.empty();
+    }
+}
